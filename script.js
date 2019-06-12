94c59cd753283539e4049af8ed720bde026a003b
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-06-12 14:37:18 -0600

    init commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+let mom=  document.getElementById('mom')
+  mom.addEventListener('click', ()=>{
+    alert('Mom....Everything is ok. I promise. Go eat some chocolate chip cookies. :) ')
+  })
+  
+// swal('hello world', 'everything is awesome')
