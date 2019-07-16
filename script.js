80b2dedac5f20f0505389afbab3d65aa12113542
Jacob Ankney <crosswalkastronaut@Jacobs-MacBook-Pro-2.local>
2019-07-16 10:41:37 -0600

    wofiej

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ let exitClick=document.getElementById('exitClick')
 
 contactMe.addEventListener('click', ()=>{
   document.getElementById('popUp').classList.remove('hidden')
+  document.getElementsByClassName('hero')[0].getElementsByTagName('section')[0].classList.add('hidden')
   
   
 })
 
 exitClick.addEventListener('click', ()=>{
 document.getElementById('popUp').classList.add('hidden')
+document.getElementsByClassName('hero')[0].getElementsByTagName('section')[0].classList.remove('hidden')
 })
